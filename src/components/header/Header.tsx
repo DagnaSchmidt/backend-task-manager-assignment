360038c9aa1eb540154ae1a422dcdd208c0d1f3b
Dagna Schmidt <dagdagi889@gmail.com>
2024-09-10 15:15:44 +0200

    style(header): tailwind classes added
    style for h1, h6 created in globals.css
    lato font added to body
    header component imported in main page
    style for header component added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div
+            className='flex justify-between items-baseline max-w-[340px] pb-2 border-b border-slate-700'
+        >
+            <h1>Sunday</h1>
+            <div
+                className='bg-slate-900 px-2 py-1 rounded-lg'
+            >
+                <h6>Jun 31</h6>
+            </div>
+        </div>
     )
 }
 
