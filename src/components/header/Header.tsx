d159cc09ca6c62253e65027e76d33e453d9b1092
Dagna Schmidt <dagdagi889@gmail.com>
2024-09-10 15:35:53 +0200

    feat(header): useDate hook created
    created useDate hook that return current day name, month name and useDate
    imported useDate to header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useDate } from '@/utils/useDate';
+
 
 const Header = () => {
+    const date = useDate();
+
     return (
         <div
             className='flex justify-between items-baseline max-w-[340px] pb-2 border-b border-slate-700'
         >
-            <h1>Sunday</h1>
+            <h1>{date.day}</h1>
             <div
                 className='bg-slate-900 px-2 py-1 rounded-lg'
             >
-                <h6>Jun 31</h6>
+                <h6>{date.monthAndDate}</h6>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
--- src/utils/useDate.ts
+++ src/utils/useDate.ts
@@ -1,1 +1,22 @@
-// useDate hook
+type TCurrentDate = {
+    day: string,
+    monthAndDate: string
+};
+
+export const useDate = (): TCurrentDate => {
+
+    const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Dec'];
+
+    const today = new Date();
+
+    const day = dayNames[today.getDay() - 1];
+    const month = monthNames[today.getMonth()];
+    const date = today.getDate();
+    const monthAndDate = `${month} ${date}`;
+
+    return {
+        day,
+        monthAndDate
+    }
+};
