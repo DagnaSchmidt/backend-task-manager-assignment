06c71944edaa29d7121fb8b2b9b1cbcba3e0e3f1
Dagna Schmidt <dagdagi889@gmail.com>
2024-09-11 12:03:28 +0200

    style(button): added style to button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -14,9 +14,15 @@ const Button = <C extends React.ElementType = "button">({
 
     return (
         <button
+            className={[
+                'rounded-xl px-3 py-2 text-slate-50',
+                type === 'add' ? 'bg-teal-600 hover:bg-teal-700' : 'bg-red-600 hover:bg-red-700'
+            ].join(' ')}
             {...rest}
         >
-            <p>
+            <p
+                className='text-base uppercase font-bold'
+            >
                 {label}
             </p>
         </button>
